5ec990666dac8a90a4f37cd984fe2c4bf6e6a61d
Benjamin Fry <benjaminfry@me.com>
2019-06-24 22:45:57 -0700

    remove unused import

--- examples/panicking/src/lib.rs
+++ examples/panicking/src/lib.rs
@@ -10,7 +10,6 @@ extern crate pg_extend;
 extern crate pg_extern_attr;
 
 use pg_extend::pg_magic;
-use pg_extend::pg_sys;
 use pg_extern_attr::pg_extern;
 // This tells Postges this library is a Postgres extension
 pg_magic!(version: pg_sys::PG_VERSION_NUM);
