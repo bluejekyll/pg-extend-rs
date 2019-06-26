08884680e3736d6ea80b0af3a647d5bbc15aeba4
Eric B. Ridge <eebbrr@gmail.com>
2019-06-26 02:01:21 -0600

    code cleanup + rustfmt

--- examples/quote_ident/src/lib.rs
+++ examples/quote_ident/src/lib.rs
@@ -5,29 +5,29 @@
 // http://opensource.org/licenses/MIT>, at your option. This file may not be
 // copied, modified, or distributed except according to those terms.
 
-extern crate pg_extern_attr;
 extern crate pg_extend;
+extern crate pg_extern_attr;
 
-use pg_extern_attr::pg_extern;
-use pg_extend::{pg_magic, pg_sys};
 use pg_extend::guard_pg;
+use pg_extend::{pg_magic, pg_sys};
+use pg_extern_attr::pg_extern;
 
 // This tells Postgres this library is a Postgres extension
 pg_magic!(version: pg_sys::PG_VERSION_NUM);
-
 
 /// The pg_extern attribute wraps the function in the proper functions syntax for C extensions
 #[pg_extern]
 pub fn rust_quote_ident(value: String) -> String {
     use std::ffi::{CStr, CString};
-    use std::os::raw::c_char;
 
     unsafe {
         guard_pg(|| {
-            let buf: *const c_char = pg_sys::quote_identifier(CString::new(value.as_str()).unwrap().as_ptr());
-            let rc = CStr::from_ptr(buf);
-            let str_slice: &str = rc.to_str().unwrap();
-            str_slice.to_owned()
+            CStr::from_ptr(pg_sys::quote_identifier(
+                CString::new(value.as_str()).unwrap().as_ptr(),
+            ))
+            .to_str()
+            .unwrap()
+            .to_owned()
         })
     }
 }
